169c63e2ccc3b55feb0194464b2c291f4fb8a35f
Samuca169 <samucabarr169@gmail.com>
2021-06-02 14:16:07 -0300

    Bank Account

---
+++ src/BusinessException/Exception.java
@@ -0,0 +1,9 @@
+package BusinessException;
+
+public class Exception extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public Exception(String msg) {
+		super(msg);
+	}
+}
